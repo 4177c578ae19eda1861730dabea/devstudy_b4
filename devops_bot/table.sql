2c90f8d6e06f87c70ecc3385e42cd1f94f63d892
root <root@gitlab.loal>
2024-09-09 13:26:38 +0300

    delete

--- devops_bot/table.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER repl_user REPLICATION PASSWORD 'P@ssw0rd';
-SELECT pg_create_physical_replication_slot('replication_slot');
-CREATE TABLE email (ID SERIAL PRIMARY KEY, emailname VARCHAR (100) NOT NULL);
-CREATE TABLE numberPhone (ID SERIAL PRIMARY KEY, numberphone VARCHAR (100) NOT NULL);
-
